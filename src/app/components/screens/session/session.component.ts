88734bb7b4278bb4dc7346abb620932d95dc2194
fany121 <61065845+fany121@users.noreply.github.com>
2025-05-05 15:38:50 -0600

    login
    funciona, error de inicio ., ya da los mensajes de error

--- src/app/components/screens/session/session.component.ts
+++ src/app/components/screens/session/session.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -19,32 +19,104 @@ import { SessionService } from '../../../services/session/session.service';
   templateUrl: './session.component.html',
   styleUrl: './session.component.css'
 })
+
+
+  /**
+   * 
+   * Configuración del componente.
+   * 
+   * @param _globalService Inyecta el servicio GlobalService para obtener las configuraciones globales.
+   * @param _sessionService Inyecta el servicio SessionService que realia peticiones a la API REST.
+   * @param _router Inyecta el enrutador de Angular.
+   * @param _formBuilder Inyecta el constructor de formularios de Angular.
+   * 
+   */
+
+
+
 export class SessionComponent {
+
+
+
   public title: string = 'SESIÓN';
   public form: FormGroup;
+  // public year: number;
   public isLoading: boolean = false;
 
 
-  
-  constructor(
-    private _formBuilder: FormBuilder,
-    private _router: Router
+
+  public constructor(
+    private _globalService: GlobalService, 
+    private _sessionService: SessionService, 
+    private _router: Router, 
+    private _formBuilder: FormBuilder
   ) {
-    this.form = this._formBuilder.group({
+    this.form = _formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
+    // this.year = this._globalService.getYear();
+
+    if (_sessionService.getCurrentUser()) {
+      _router.navigate(['/panel']);
+    }
   }
 
-  public login(): void {
-    // if (this.form.valid) {
-    //   const { username, password } = this.form.value;
-    //   console.log('Formulario válido', username, password);
-    //   // Redirigir a "panel" o simular login:
-    //   // this._router.navigate(['/panel']);
-    // } else {
-    //   console.warn('Formulario inválido');
-    //   this.form.markAllAsTouched();
-    // }
+/**
+   * 
+   * Método que consulta al servicio SessionService la autenticación de un usuario
+   * pasando el nombre de usuario y contraseña al mismo a la API REST.
+   * Si todo sale bien guarda el usuario que ha iniciado sesíon y redirige al usuario 
+   * a la ventana de Home.
+   * 
+   */
+public login(): void {
+  this.isLoading = true;
+
+  if (this.form.valid) {
+    this._sessionService.login({
+      id_user: 0,
+      full_name: '',
+      username: this.form.get('username')!.value,
+      password: this.form.get('password')!.value,
+      password_old: '',
+      email: '',
+      date_created: '',
+      hash_username: '',
+      hash_password: '',
+      state: '',
+      role: '',
+      created_by: '',
+      id_state: 0,
+      id_role: 0,
+      id_created_by: 0
+    }).subscribe({
+      next: (value: Response) => {
+        if (value.code == 202) {
+          this._sessionService.setCurrentUser(value.data);
+          this._router.navigate(['/panel']);
+          this._globalService.showToast(this.title, 'Inicio de sesión exitoso.', TypeToast.SUCCESS, 'checkmark-circle');
+        } else if (value.code == 401) {
+          this.isLoading = false;
+          this._globalService.showToast(this.title, 'Usuario o contraseña incorreto, intente de nuevo.', TypeToast.DANGER, 'finger-print');
+        } else {
+          this.isLoading = false;
+          this._globalService.showToast(this.title, 'Ha ocurrido un error.', TypeToast.DANGER, 'code-slash');
+        }
+      },
+      error: (error: any) => {
+        this.isLoading = false;
+        this._globalService.showToast(this.title, 'Ha ocurrido un error.', TypeToast.DANGER, 'code-slash');
+      }
+    });
+  } else {
+    Object.keys(this.form.controls).forEach((field: string) => {
+      const control: AbstractControl<any, any> | null = this.form.get<string>(field);
+      if (control && control.invalid) control.markAsTouched({ onlySelf: true });
+    });
+
+    this.isLoading = false;
+    this._globalService.showToast(this.title, 'Formulario incompleto.', TypeToast.DANGER, 'code-slash');
   }
+}
 }
